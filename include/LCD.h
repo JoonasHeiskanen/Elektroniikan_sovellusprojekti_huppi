0fa9edb96509f3f2b77fa7041b76534bc9b6fd4a
harkonennen <harri.antti.harkonen@gmail.com>
2026-04-13 10:39:13 +0300

    working module with float print function

--- include/LCD.h
+++ include/LCD.h
@@ -1,27 +1,7 @@
-#ifndef __LCD_H__
-#define __LCD_H__
+#ifndef LCD_H
+#define LCD_H
 
 void lcd_setup(void);
-
-#pragma once
-
-#define ILI9341_2_DRIVER
-
-#define TFT_CS   4
-#define TFT_RST  5
-#define TFT_DC   6
-#define TFT_MOSI 7
-#define TFT_SCLK 15
-#define TFT_MISO 17
-
-#define LOAD_GLCD
-#define LOAD_FONT2
-#define LOAD_FONT4
-#define LOAD_GFXFF
-
-#define TFT_INVERSION_ON
-
-#define SPI_FREQUENCY       10000000
-#define SPI_READ_FREQUENCY  5000000
+void lcd_printToScreen(float Data);
 
 #endif
--- src/LCD.cpp
+++ src/LCD.cpp
@@ -1,15 +1,29 @@
-#include <LCD.h>
+#include <Arduino.h>
+#include "lcd.h"
 #include <TFT_eSPI.h>
 
 TFT_eSPI tft = TFT_eSPI();
 
 void lcd_setup() {
-  Serial.begin(115200);
-  delay(1000);
+  Serial.println("lcd_setup start");
+
+  #ifdef USER_SETUP_INFO
+    Serial.println(USER_SETUP_INFO);
+  #else
+    Serial.println("USER_SETUP_INFO not found");
+  #endif
+
   tft.init();
   tft.setRotation(2);
-  tft.fillScreen(0x0129);
-  tft.setTextColor(TFT_WHITE);
+  tft.fillScreen(TFT_BLUE);
+  tft.setTextColor(TFT_WHITE, TFT_BLUE);
   tft.drawString("hello world!", 0, 0, 2);
-  tft.drawString("lcd_v0.1", 0, 15, 2);
+
+  Serial.println("lcd setup done");
+}
+
+void lcd_printToScreen(float Data){
+  tft.fillScreen(TFT_CYAN);
+  tft.setTextColor(TFT_WHITE,TFT_BLACK);
+  tft.drawFloat(Data,2,0,0);
 }
